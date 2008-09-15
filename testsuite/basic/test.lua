2145f92940e8c674dd8705c5d7113da49ee51047
alessandrohc <alessandrohc>
2008-09-15 13:21:46 +0000

    test refactoring

--- testsuite/basic/test.lua
+++ testsuite/basic/test.lua
@@ -1,20 +1,21 @@
 package.path = [[;;./net.luaforge.loft/source/lua/5.1/?.lua;./net.luaforge.loft/source/lua/5.1/?/init.lua;]]
 
-
 require "schema"
 
 local allTypes = Schema 'Cli' {
-
+	
+	--[[
 	Data 'TipoDocumento' {
 	 	CPF = "Cadastro de Pessoa Fisica",
 	 	RG = { nome = "Identidadade", description="Registro Geral" },
 	};
-
+	
 	Data 'Estado' {
 		{id=1,name='Novo'}, 
 		{id=2,name='Criado'}, 
 		{id=3,name='Alterado'}
 	};
+	--]]
 	
 	Type 'Endereco'
 		. rua
@@ -22,13 +23,14 @@ local allTypes = Schema 'Cli' {
 		. complemento
 		. bairro
 		. cidade
-		. cep
+		. cep;
 
 	Type 'PessoaFisica'
 		. nome
 		. endereco
-		. telefone
-
+		. telefone;
+	
+	--[[
 	Type 'Cliente' {tableName='tbCliente', databaseName='teste'}
 		. nome'Nome'{required=true}
 		. endereco
@@ -36,17 +38,17 @@ local allTypes = Schema 'Cli' {
 		. cep'clCep' : Number{size=256, required=true }
 		. idade 
 		. dataNascimento : Date('now')
-		. tipoDocumento : Reference('tipoDocumento');
+		. tipoDocumento : Reference('tipoDocumento')
 		. presidente : Reference('passoaFisica');
 		
 	Type 'Assinatura'
 	 	. tipo {
 	 		Type 'TipoAssinatura'
 	 			. nome
-	 			. descricao
-	 	}
-	}
-	
+	 			. descricao;
+	 	};
+	--]]
+	--[[
 	Validations {
 		'PessoaFisica' {
 			.idade = function(field, oldvalue, newvalue)
@@ -59,14 +61,15 @@ local allTypes = Schema 'Cli' {
 				return true, {}, {}
 			end
 		}
-	}
-
+	};
+	
 	Indexes {
 		'standard' = Filterable {
 			fields = {'nome', 'cpf'}
 		}
-	}
-
+	};
+	--]]
+}
 
 local newsTypes = Schema 'Newsletter' {
 	Type 'Grupo'
@@ -81,53 +84,16 @@ local newsTypes = Schema 'Newsletter' {
 				warning()
 			end;
 		}
-		.from
-		.groups : Collection(Type'Grupo')
-		
+		.from;
+--		.groups : Collection(Type'Grupo');		
 }
 
-
-t = newsTypes['Veiculo'].fields().names()
-t = newsTypes['Veiculo'].fields().types()
-
-loft.initialize('sqlite3')
-
-loft.registerSchema(newsTypes)
-
-v = new'Veiculo'
-
-if v:validate() then
-
-loft.persist(v)
-
-else
-
-local msgs = {
-	unpack(v:getErrors()),
-	unpack(v:getWarnings())
-}
-
-end
-
---[[-----------------
-
-vf = Face 'ListaDeVeiculos' For 'Veiculo' {
-	.nome 
-	.teste {fieldName='descricao', size=70}
-	.valor {type='currency', currency='BRL'}
-	.dataNascimento {type='date', format='DD/MM/AA'}
-	.grupo {
-		type='collection',
-		items = {
-			nome = {type='text'}
-		}
-	}
-}
-
---]]-----------------
+--t = newsTypes['Veiculo'].fields().names()
+--t = newsTypes['Veiculo'].fields().types()
 
 
-for typeName, t in pairs(allTypes) do
+print("\n\n\n\n")
+for typeName, t in pairs(newsTypes) do
 	print'---------------------------'
 	print(t['.typeName'])
 	print'---------------------------'
